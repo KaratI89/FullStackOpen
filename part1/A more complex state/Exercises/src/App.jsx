21e13470d2400dd50981a324735ed9dff71de506
KaratI89 <karat0zero@gmail.com>
2023-09-09 19:54:50 +0300

    moving

--- part1/A more complex state/Exercises/src/App.jsx
+++ part1/A more complex state/Exercises/src/App.jsx
@@ -4,32 +4,49 @@ const Button =(props) => (
   <button onClick={props.handleClick}>{props.text}</button>
 )
 
+const StatisticLine = (props) => {
+return(
+  <tr>
+    <td>{props.text}: </td>
+    <td>{props.value} </td>
+  </tr>
+)
+}
+
 const Statistics =(props) => {
 const rating = [props.good, props.neutral, props.bad]
+const [good, neutral, bad] = rating
 const sumRatings = rating.reduce((sum , current) => sum + current, 0)
 const averageRatings = () => {
-  const quotient = (rating[0]-rating[2]) / sumRatings
+  const quotient = (good-bad) / sumRatings
   if (!quotient) {
     return 0
   }
   return quotient
 }
 const goodPercent = () => {
-  const quotient = rating[0] / sumRatings *100
+  const quotient = good / sumRatings *100
   if (!quotient) {
     return 0
   }
-  return quotient
+  return (`${quotient} %`)
+}
+if (sumRatings === 0) {
+  return (
+    <h3>No feedback given</h3>
+  )
 }
 return (
-  <>
-  good: {rating[0]}
-  <br/>neutral: {rating[1]} 
-  <br/>bad: {rating[2]} 
-  <br/>all: {sumRatings}
-  <br/>average: {averageRatings()}
-  <br/>positive: {goodPercent()}
-  </>
+  <table>
+    <tbody>
+    <StatisticLine text = {'good'} value = {good} />
+    <StatisticLine text = {'neutral'} value = {neutral} />
+    <StatisticLine text = {'bad'} value = {bad} />
+    <StatisticLine text = {'all'} value = {sumRatings} />
+    <StatisticLine text = {'average'} value = {averageRatings()} />
+    <StatisticLine text = {'positive'} value = {goodPercent()} />
+    </tbody>
+  </table>
 )
 }
 
@@ -39,22 +56,22 @@ const App =() => {
   const [bad, setBad] = useState(0)
 
   const handleClickGood = (click) => {
-    setGood(click)
+    setGood(click + 1)
   }
 
   const handleClickNeutral = (click) => {
-    setNeutral(click)
+    setNeutral(click + 1)
   }
 
   const handleClickBad = (click) => {
-    setBad(click)
+    setBad(click + 1)
   }
   return (
     <div>
       <h1>give feedback</h1>
-    <Button handleClick={() => handleClickGood(good + 1)} text='good' />
-    <Button handleClick={() => handleClickNeutral(neutral + 1)} text='neutral' />
-    <Button handleClick={() => handleClickBad(bad + 1)} text='bad' />
+    <Button handleClick={() => handleClickGood(good)} text='good' />
+    <Button handleClick={() => handleClickNeutral(neutral)} text='neutral' />
+    <Button handleClick={() => handleClickBad(bad)} text='bad' />
     <h2>statistics</h2>
     <Statistics good={good} neutral={neutral} bad={bad} />
     </div>
