88bb5266828c24abde52ea4e8ee2e5677d3cd322
KaratI89 <karat0zero@gmail.com>
2023-10-02 21:50:34 +0300

    exercise 2.11 is completed

---
+++ part2/getting data from server/exercises/phonebook/src/App.jsx
@@ -0,0 +1,106 @@
+import { useState, useEffect } from "react"
+import { Person } from "./components/Name"
+import axios from "axios"
+
+const Filter = (props) => {
+  return (
+    <div>
+    filter shown with:
+    <input
+      value={props.filter}
+      onChange={props.handleFilterChange}
+    />
+  </div>
+  )
+}
+
+const PersonForm = (props) => {
+  return(
+    <form onSubmit={props.addPerson}>
+    <div>
+      name:
+      <input
+        value={props.newName}
+        onChange={props.handleNameChange} 
+      />
+    </div>
+    <div>
+      number:
+      <input
+        value={props.newNumber}
+        onChange={props.handleNumberChange}
+      />
+    </div>
+    <div>
+      <button type='submit'>add</button>
+    </div>
+  </form>
+
+  )
+}
+
+export const App = () => {
+  const [persons, setPersons] = useState([])
+  const [newName, setNewName] = useState('enter a new name')
+  const [newNumber, setNewNumber] = useState('enter a number')
+  const [filter, setFilter] = useState('')
+
+  useEffect(() => {
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        setPersons(response.data)
+      })
+  },[])
+  
+
+  const addPerson = (event) => {
+    event.preventDefault()
+    let numb = persons.map(person => person.name).indexOf(newName)
+    console.log(numb);
+    if (numb === -1) {
+      setPersons(persons.concat({ name: newName, number: newNumber})) 
+    }
+    else{
+      window.alert(`${newName} is already added to phonebook`)
+    }
+    setNewName('')
+    setNewNumber('')
+    console.log('this is the form', event.target)
+  }
+
+  const handleNameChange = (event) => {
+    setNewName(event.target.value)
+  }
+
+  const handleNumberChange = (event) => {
+    setNewNumber(event.target.value)
+  }
+
+  const handleFilterChange = (event) => {
+    setFilter(event.target.value)
+  }
+
+  const filteredPersons = persons.filter(person => person.name.toLowerCase().includes(filter.toLowerCase()))
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <Filter filter={filter} handleFilterChange={handleFilterChange} />
+      <h3>Add new adreses</h3>
+      <PersonForm 
+        addPerson={addPerson} 
+        newName={newName} 
+        handleNameChange={handleNameChange} 
+        newNumber={newNumber} 
+        handleNumberChange={handleNumberChange}
+      />
+      <h2>Numbers</h2>
+      <ul>
+        {filteredPersons.map((person) => <Person key={person.name} person={person} />
+        )}
+      </ul>
+    </div>
+  )}
+
+export default App
